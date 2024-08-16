dec4a7020ca132b79b4cdcc1dc763d415b771d9a
Jyoti Hambir <qdde3t1v@students.codeinstitute.net>
2024-08-16 12:20:08 +0000

    add questions with answer options in js file

---
+++ assets/js/script.js
@@ -0,0 +1,94 @@
+const questions = [
+    {
+        question: "What is the most consumed fruit in the world?",
+        answers: [
+            {text: "Apple", correct: false},
+            {text: "Banana", correct: true},
+            {text: "Orange", correct: false},
+            {text: "Grapes", correct: false},
+        ]
+    },
+    {
+        question: "Which country is responsible for creating hot sauce?",
+        answers: [
+            {text: "Australia", correct: false},
+            {text: "India", correct: false},
+            {text: "Mexico", correct: true},
+            {text: "Argentina", correct: false},
+        ] 
+    },
+    {
+        question: "In which country did the Caesar salad originate?",
+        answers: [
+            {text: "Mexico", correct: true},
+            {text: "Bangladesh", correct: false},
+            {text: "England", correct: false},
+            {text: "Bahamas", correct: false},
+        ]  
+    },
+    {
+        question: "What is the main ingredient in hummus?",
+        answers: [
+            {text: "Lemon Juice", correct: false},
+            {text: "Chickpeas", correct: true},
+            {text: "Olive Oil", correct: false},
+            {text: "Garlic", correct: false},
+        ]  
+    },
+    {
+        question: "Which popular condiment is made from fermented soybeans?",
+        answers: [
+            {text: "Soy Sauce", correct: true},
+            {text: "Vinegar", correct: false},
+            {text: "Hot Sauce", correct: false},
+            {text: "Ketchup", correct: false},
+        ]  
+    },
+    {
+        question: "What is the most expensive spice in the world by weight?",
+        answers: [
+            {text: "Cummin", correct: false},
+            {text: "Clove", correct: false},
+            {text: "Cardamom", correct: false},
+            {text: "Saffron", correct: true},
+        ]  
+    },
+    {
+        question: "Which vegetable has the highest water content?",
+        answers: [
+            {text: "Water melon", correct: false},
+            {text: "Tomato", correct: false},
+            {text: "Cucumber", correct: true},
+            {text: "Onion", correct: false},
+        ]  
+    },
+    {
+        question: "Which food establishment has the most number of stores in the world?",
+        answers: [
+            {text: "KFC", correct: false},
+            {text: "McDonald’s", correct: true},
+            {text: "Burger King", correct: false},
+            {text: "Starbucks", correct: false},
+        ]  
+    },
+    {
+        question: "What is the name of the sweet, tangy fruit used to flavour Worcestershire sauce?",
+        answers: [
+            {text: "Lemon", correct: false},
+            {text: "Orange", correct: false},
+            {text: "Tamarind", correct: true},
+            {text: "Kiwi", correct: false},
+        ]  
+    },
+    {
+        question: "Which type of tea is known as the ‘champagne of teas’ due to its delicate flavour and high price?",
+        answers: [
+            {text: "Darjeeling tea", correct: true},
+            {text: "Green tea", correct: false},
+            {text: "Herbal tea", correct: false},
+            {text: "Chamomile tea", correct: false},
+        ]  
+    }
+
+
+]
